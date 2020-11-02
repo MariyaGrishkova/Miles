5a779db0ee7344e940b0eddce164fc757b72f1c0
Mariya Grishkova <galeyevamv@gmail.com>
2020-11-02 09:40:52 +0300

    init

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        Long ticket = 20_000L;
+        float index = 0.05F;
+
+        long Miles;
+        Miles = (long) (ticket * index);
+        System.out.println(Miles);
+    }
+}
