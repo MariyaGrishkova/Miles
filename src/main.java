600cef1ec98490a80796e352ba47f637b7a5eaf7
Mariya Grishkova <galeyevamv@gmail.com>
2020-11-05 09:46:15 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,7 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        boolean registered = true;
-        Long ticket = 20_000L;
+        long ticket = 20_000L;
         float index = 0.05F;
 
         long Miles;
