7a9fab5d2aa03a22a49a74cb412299ae305d482c
Mariya Grishkova <galeyevamv@gmail.com>
2020-11-12 15:23:10 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,12 @@
 public class BonusMilesService {
-    public int calculate (int amount) {
-    long ticket = 20_000L;
-    float index = 0.05F;
+    public int calculate(int amount) {
+        long ticket = 20_000L;
+        float index = 0.05F;
 
-    int Miles;
-    Miles = (int) (ticket * index);
+        int miles;
+        miles = (int) (ticket * index);
 
-    return Miles;
+        return miles;
     }
 }
 
